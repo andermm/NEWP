6f8415dc6f99e59901657aa9a82dfb41891a443c
Anderson <andersonmaliszewski@gmail.com>
2020-10-15 23:53:29 -0300

    read updates

--- SH/read.sh
+++ SH/read.sh
@@ -24,8 +24,31 @@ MACHINEFILE64=$MACHINE_FILES/nodes64
 
 
 #Kill processes
-kill -15 -1
+#kill -15 -1
 
+#Exec
+cd $BENCHMARKS
+wget -c https://www.nas.nasa.gov/assets/npb/NPB3.4.1.tar.gz 
+tar -xzf NPB3.4.1.tar.gz
+rm -rf NPB3.4.1.tar.gz
+
+for f in $APP_CONFIG_NPBE/*.def.template; do
+        mv -- "$f" "${f%.def.template}.def";
+done
+
+sed -i 's,mpif90,mpifort,g' $APP_CONFIG_NPBE/make.def
+appsn=(bt sp is ft)
+classes=(C)
+echo -n "" > $APP_CONFIG_NPBE/suite.def
+
+for (( n = 0; n < 4; n++ )); do
+        for (( i = 0; i < 1; i++ )); do
+                echo -e ${appsn[n]}"\t"${classes[i]} >> $APP_CONFIG_NPBE/suite.def
+        done
+done
+cd $APP_COMPILE_NPBE; make suite
+
+####################################################################################################################
 
 cd $DIR
 
