746e26e0d88528f1c3a4e1d0a6cc5104aca7e0ff
Anderson <andersonmaliszewski@gmail.com>
2020-10-21 21:34:13 -0300

    update

--- SH/read.sh
+++ SH/read.sh
@@ -18,122 +18,88 @@ APP_BIN_NPBE=$NPBE/NPB3.4-MPI/bin/
 app_procs=(is_1 ft_31 is_61 ft_91 bt_121 sp_151 bt_181 sp_211 bt_241 sp_271 bt_301 sp_331)
 
 cd $DIR
-c=1
 
-for (( a = 0; a < 12; a++ )); do
-	app_proc=${app_procs[a]}
-	while [[ $c < 2 ]]; do
-		if [[ -e $app_proc\_16 ]]; then
-			let c++
-		elif [[ -e $app_proc\_64 ]]; then
-			let c++
-		fi
-	done
-done
-
-
-for (( p = 0; p < 2; p++ )); do
-	processes=${processesn[p]}
 	for (( n = 0; n < 4; n++ )); do
 		app=${appsn[n]}
-			for i in {1..331..30}; do 
+		for i in {1..331..30}; do 
 				FILE_[$i]="$app"_"$i"
 				if [[ -e "${FILE_[i]}"\_16 ]]; then
 					touch $slaveok
-										
+					MACHINEFILE=$MACHINEFILE16
+
 					while [[ true ]]; do
-					if [[ "${FILE_[i]}"_$processes == ${appsn[2]}_1_16 ]]; then
+					if [[ "${FILE_[i]}"_16 == ${appsn[2]}_1_16 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[3]}_31_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[3]}_31_16 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[2]}_61_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[2]}_61_16 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[3]}_91_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[3]}_91_16 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_121_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[0]}_121_16 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_151_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[1]}_151_16 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_181_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[0]}_181_16 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_211_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[1]}_211_16 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_241_16 ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[0]}_241_16 ]]; then
 						KERNEL=bt.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_271_16  ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[1]}_271_16  ]]; then
 						KERNEL=sp.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_301_16  ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[0]}_301_16  ]]; then
 						KERNEL=sp.C.x
-						MACHINEFILE=$MACHINEFILE16
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_331_16  ]]; then
+					elif [[ "${FILE_[i]}"_16 == ${appsn[1]}_331_16  ]]; then
 						KERNEL=bt.C.x
-						MACHINEFILE=$MACHINEFILE16
 					fi
-				fi
 
 					date
 					mpiexec --mca btl self,tcp \
 							--mca btl_tcp_if_include eth0 \
-							-np $processesn \
+							-np 16 \
 							-machinefile $MACHINEFILE \
 							"$BENCHMARKS/$APP_BIN_NPBE"$KERNEL
 					done
 
-				if [[ -e "${FILE_[i]}"\_64 ]]; then
+				elif [[ -e "${FILE_[i]}"\_64 ]]; then
 					touch $slaveok
-					
-					while [[ true ]]; do									
-					if [[ "${FILE_[i]}"_$processes == ${appsn[2]}_1_64 ]]; then
+					MACHINEFILE=$MACHINEFILE64
+					while [[ true ]]; do
+									
+					if [[ "${FILE_[i]}"_64 == ${appsn[2]}_1_64 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[3]}_31_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[3]}_31_64 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[2]}_61_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[2]}_61_64 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[3]}_91_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[3]}_91_64 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_121_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[0]}_121_64 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_151_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[1]}_151_64 ]]; then
 						KERNEL=is.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_181_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[0]}_181_64 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_211_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[1]}_211_64 ]]; then
 						KERNEL=ft.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_241_64 ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[0]}_241_64 ]]; then
 						KERNEL=bt.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_271_64  ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[1]}_271_64  ]]; then
 						KERNEL=sp.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[0]}_301_64  ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[0]}_301_64  ]]; then
 						KERNEL=sp.C.x
-						MACHINEFILE=$MACHINEFILE64
-					elif [[ "${FILE_[i]}"_$processes == ${appsn[1]}_331_64  ]]; then
+					elif [[ "${FILE_[i]}"_64 == ${appsn[1]}_331_64  ]]; then
 						KERNEL=bt.C.x
-						MACHINEFILE=$MACHINEFILE64
 					fi
-				fi
+
+					date
+					mpiexec --mca btl self,tcp \
+							--mca btl_tcp_if_include eth0 \
+							-np 64 \
+							-machinefile $MACHINEFILE \
+							"$BENCHMARKS/$APP_BIN_NPBE"$KERNEL
+					done
+				fi	
 			done
-					
-			fi
-		done
-	done
-done
+		done	
